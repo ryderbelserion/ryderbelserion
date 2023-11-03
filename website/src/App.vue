49c37cc7381260a83a2b9a853bf4177276277714
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-02 23:39:31 -0400

    add vue project

---
+++ website/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Hello</h1>
+
+      <!---<nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>-->
+    </div>
+  </header>
+
+  <!---<RouterView />-->
+</template>
