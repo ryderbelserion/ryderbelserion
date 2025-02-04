df3eec1a5978db85a736b693f67610f7a83d3431
Ryder Belserion <contact@ryderbelserion.com>
2025-02-04 03:33:38 -0500

    Add header class

--- src/layouts/layout.vue
+++ src/layouts/layout.vue
@@ -30,9 +30,45 @@ useSeoMeta({
 </script>
 
 <template>
+  <header>
+    <div class="buttons">
+      <a href="/" class="button">Home</a>
+      <a href="https://discord.gg/w7yCw4M9za" class="button">Discord</a>
+    </div>
+  </header>
+
   <!--<audio autoplay id="bg-music">
     <source src="/audio.ogg" type="audio/ogg">
   </audio>-->
 
-  <Footer/>
+  <h1>A cold beer after a hard day's work... Nothin' like it</h1>
+
+  <!--<Footer/>-->
 </template>
+
+<style scoped>
+  header {
+    display: flex;
+
+    height: 2.1em;
+  }
+
+  .buttons {
+    display: inherit;
+
+    align-content: center;
+  }
+
+  .button {
+    display: inherit;
+
+    padding: 10px 5px;
+
+    user-select: none;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 0.8em;
+  }
+</style>
