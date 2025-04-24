9198f52437de04bf3f67b0ac1c04c6b35c0022b2
Ryder Belserion <contact@ryderbelserion.com>
2025-04-23 22:07:47 -0400

    Remove commented code

--- src/error.vue
+++ src/error.vue
@@ -53,20 +53,3 @@ p {
   font-size: 16px;
 }
 </style>
-
-<!--<script setup lang="ts">
-import type { NuxtError } from '#app'
-
-const props = defineProps({
-  error: Object as () => NuxtError
-})
-
-const handleError = () => clearError({ redirect: '/' })
-</script>
-
-<template>
-  <div>
-    <h2>{{ error.statusCode }}</h2>
-    <button @click="handleError">Clear errors</button>
-  </div>
-</template>-->
