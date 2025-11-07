78bc0d8b3bce786f44abc610d3863e0747d81d01
Ryder Belserion <contact@ryderbelserion.com>
2025-11-06 22:00:18 -0500

    Update project structure

--- app/layouts/layout.vue
+++ app/layouts/layout.vue
@@ -31,18 +31,6 @@ useSeoMeta({
   twitterCard: "summary_large_image",
 })
 
-/* unless you need to replace these on each page, you can make them default in nuxt.config.ts */
-/*useHead({
-  charset: 'utf-8',
-  viewport: 'width=device-width, initial-scale=1',
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-  ]
-})*/
-
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      htmlAttrs: {
-        lang: 'en'
-      },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
-    }
-  },
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
 
-  srcDir: 'src',
+            htmlAttrs: {
+                lang: 'en'
+            },
 
-  ssr: true,
-
-  css: ['~/assets/css/global.css'],
-
-  /* makes working with scss more optimized */
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern'
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    href: '/favicon.ico'
+                }
+            ]
         }
-      }
-    }
-  }
+    },
+
+    compatibilityDate: '2025-07-15',
+
+    devtools: {
+        enabled: true
+    },
+
+    image: {
+        format: ['webp', 'gif'],
+        inject: true
+    },
+
+    ssr: true,
+
+    css: ['~/assets/css/global.css'],
+
+    site: {
+        url: 'https://ryderbelserion.com',
+        name: "Ryder's Tavern"
+    },
+
+    robots: {
+        blockNonSeoBots: true
+    },
+
+    /* makes working with scss more optimized */
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern"
+                },
+            },
+        },
+    },
+
+    modules: ['@nuxt/image', '@nuxtjs/sitemap', '@nuxtjs/robots', '@nuxt/icon']
 })
